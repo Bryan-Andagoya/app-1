cbc477a93973332610c85a872abaa9889e3885ec
Bryan-an <bryanandagoya@gmail.com>
2023-01-16 21:06:31 -0500

    hello world

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
       <StatusBar style="auto" />
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>My first app</Text>
+      </View>
+      <View style={styles.bodyContainer}>
+        <Text>Hello World</Text>
+      </View>
     </View>
   );
 }
@@ -15,6 +19,18 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
+    justifyContent: "flex-start",
+    paddingTop: StatusBar.currentHeight,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  titleContainer: {
+    paddingBottom: 10,
+  },
+  bodyContainer: {
+    flex: 1,
     justifyContent: "center",
   },
 });
